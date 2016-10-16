43360ecfd8d9c399a57c3e5141b32e73fc941087
rafiraharjo <rafiraharjo@yahoo.co.id>
2016-10-16 09:51:56 +0700

    second commit : penambahan validasi form

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1027/pendaftaranendurocross/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1027/pendaftaranendurocross/MainActivity.java
@@ -34,7 +34,10 @@ public class MainActivity extends AppCompatActivity {
         alamat = (EditText) findViewById(R.id.alamat);
         tgl = (EditText) findViewById(R.id.tgl_lahir);
         findViewById(R.id.submit).setOnClickListener(new View.OnClickListener() {
-            doclick();
+            @Override
+            public void onClick(View v) {
+                doclick();
+            }
         });
     }
 
@@ -55,11 +58,47 @@ public class MainActivity extends AppCompatActivity {
             if (htl.isChecked()) fasilitas += htl.getText() + "\n";
 
             if (fasilitas.length() == leng) fasilitas += "Tidak ada pada pilihan";
-            tvhasil.setText("Pendaftaran Berhasil! \n" + "Nama : " + nama + "\nTanggal Lahir : " + tgl +
-                    "\nAlamat : " + alamat + "\nJenis Kelamin : " + jk + "\nKelas : " + kelas.getSelectedItem().toString() +
+            tvhasil.setText("Pendaftaran Berhasil! \n" + "Nama : " + name + "\nTanggal Lahir : " + lahir +
+                    "\nAlamat : " + almt + "\nJenis Kelamin : " + jk + "\nKelas : " + kelas.getSelectedItem().toString() +
                     "\n" + fasilitas + "\n\n\t\tSelamat Begabung di Enduro Cross 2016!");
         }
 
     }
 
+    private boolean isValid() {
+        boolean valid = true;
+        String name = nama.getText().toString();
+        String almt = alamat.getText().toString();
+        String lahir = tgl.getText().toString();
+
+        if (name.isEmpty()) {
+            nama.setError("Nama Belum Diisi!");
+            valid = false;
+        } else if (name.length() < 3) {
+            nama.setError("Nama Minimal 3 Huruf!");
+            valid = false;
+        } else {
+            nama.setError(null);
+        }
+
+        if (almt.isEmpty()) {
+            alamat.setError("Alamat Belum Diisi!");
+            valid = false;
+        } else {
+            alamat.setError(null);
+        }
+
+        if (lahir.isEmpty()) {
+            tgl.setError("Tanggal Lahir Belum Diisi!");
+            valid = false;
+        } else {
+            tgl.setError(null);
+        }
+
+        if ((!rbPR.isChecked()) && (!rbWN.isChecked())) {
+            valid = false;
+        }
+
+        return valid;
+    }
 }
